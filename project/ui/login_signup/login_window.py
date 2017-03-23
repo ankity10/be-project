8614d88c9fe9d5d3889fa02bf2381fd45facb204
Ankit yadav <ankit@localhost.localdomain>
2017-03-23 22:17:46 +0530

    Adding improved ui for login/signup

--- project/ui/login_signup/login_window.py
+++ project/ui/login_signup/login_window.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Login_Window.ui'
+# Form implementation generated from reading ui file 'login_window.ui'
 #
-# Created by: PyQt5 UI code generator 5.7
+# Created by: PyQt5 UI code generator 5.8
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_Login_Window(object):
     def setupUi(self, Login_Window):
         Login_Window.setObjectName("Login_Window")
-        Login_Window.resize(358, 265)
+        Login_Window.resize(429, 298)
         palette = QtGui.QPalette()
         brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
         brush.setStyle(QtCore.Qt.SolidPattern)
@@ -160,96 +159,109 @@ class Ui_Login_Window(object):
         Login_Window.setWindowOpacity(2.0)
         Login_Window.setAutoFillBackground(False)
         Login_Window.setStyleSheet("QDialog{\n"
-                                   "background-color : rgb(255, 255, 255);\n"
-                                   "border-color : rgb(66, 133, 200);\n"
-                                   "border-size : 5px;\n"
-                                   "    font:  14pt \"Roboto\";\n"
-                                   "}")
+"background-color : rgb(255, 255, 255);\n"
+"border-color : rgb(66, 133, 200);\n"
+"border-size : 5px;\n"
+"    font:  14pt \"Roboto\";\n"
+"}")
         self.login_button = QtWidgets.QPushButton(Login_Window)
-        self.login_button.setGeometry(QtCore.QRect(60, 140, 251, 27))
+        self.login_button.setGeometry(QtCore.QRect(90, 170, 251, 41))
         self.login_button.setAutoFillBackground(False)
         self.login_button.setStyleSheet("QPushButton{\n"
-                                        "    \n"
-                                        "    font: 11pt \"Roboto\";\n"
-                                        "background-color : rgb(79, 159, 238);\n"
-                                        "    border-color: rgb(55, 111, 166);\n"
-                                        "}")
+"    color: #fff;\n"
+"    font: 11pt \"Roboto\";\n"
+"background-color : rgb(79, 159, 238);\n"
+"    border-color: blue;\n"
+"border-radius: 4px;       \n"
+"  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);\n"
+"  transition: all 0.3s cubic-bezier(.25,.8,.25,1);\n"
+"\n"
+"\n"
+"\n"
+"       \n"
+"}")
         self.login_button.setObjectName("login_button")
         self.username = QtWidgets.QLineEdit(Login_Window)
-        self.username.setGeometry(QtCore.QRect(60, 60, 251, 27))
+        self.username.setGeometry(QtCore.QRect(90, 60, 251, 31))
+        self.username.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.username.setAutoFillBackground(False)
         self.username.setStyleSheet("QLineEdit\n"
-                                    "{\n"
-                                    "    background-color: rgb(184, 184, 184);\n"
-                                    "    border-color: rgb(63, 127, 190);\n"
-                                    "    font: 11pt \"Roboto\";\n"
-                                    "}")
+"{\n"
+"    background-color: rgb(184, 184, 184);\n"
+"    border-color: rgb(63, 127, 190);\n"
+"    font: 11pt \"Roboto\";\n"
+"border-radius: 4px;\n"
+"}")
         self.username.setFrame(False)
+        self.username.setAlignment(QtCore.Qt.AlignCenter)
         self.username.setObjectName("username")
         self.password = QtWidgets.QLineEdit(Login_Window)
-        self.password.setGeometry(QtCore.QRect(60, 100, 251, 27))
+        self.password.setGeometry(QtCore.QRect(90, 110, 251, 31))
         self.password.setStyleSheet("QLineEdit\n"
-                                    "{\n"
-                                    "    background-color: rgb(184, 184, 184);\n"
-                                    "    font: 11pt \"Roboto\";\n"
-                                    "}")
+"{\n"
+"    background-color: rgb(184, 184, 184);\n"
+"    font: 11pt \"Roboto\";\n"
+"border-radius: 4px;\n"
+"}")
         self.password.setFrame(False)
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.password.setAlignment(QtCore.Qt.AlignCenter)
         self.password.setObjectName("password")
-        self.new_user_link = QtWidgets.QCommandLinkButton(Login_Window)
-        self.new_user_link.setGeometry(QtCore.QRect(130, 200, 101, 31))
-        self.new_user_link.setStyleSheet("QCommandLinkButton\n"
-                                         "{\n"
-                                         "    qproperty-icon : None;\n"
-                                         "    color: rgb(67, 135, 203);\n"
-                                         "    text-align : center;\n"
-                                         "    font: 11pt \"Roboto\";\n"
-                                         "}")
-        self.new_user_link.setObjectName("new_user_link")
+        self.login_label = QtWidgets.QLabel(Login_Window)
+        self.login_label.setGeometry(QtCore.QRect(150, 10, 131, 41))
+        self.login_label.setStyleSheet("QLabel\n"
+"{\n"
+"    color: rgb(67, 135, 202);\n"
+"    \n"
+"    \n"
+"    font: 14pt \"Roboto\";\n"
+"}")
+        self.login_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.login_label.setObjectName("login_label")
+        self.signup_link = QtWidgets.QPushButton(Login_Window)
+        self.signup_link.setGeometry(QtCore.QRect(120, 240, 191, 41))
+        self.signup_link.setText("Not registered - Signup")
+        self.signup_link.setObjectName("signup_link")
+        self.signup_label = QtWidgets.QLabel(Login_Window)
+        self.signup_label.setGeometry(QtCore.QRect(150, 10, 131, 41))
+        self.signup_label.setStyleSheet("QLabel\n"
+"{\n"
+"    color: rgb(67, 135, 202);\n"
+"    \n"
+"    \n"
+"    font: 14pt \"Roboto\";\n"
+"}")
+        self.signup_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.signup_label.setObjectName("signup_label")
+        self.login_link = QtWidgets.QPushButton(Login_Window)
+        self.login_link.setGeometry(QtCore.QRect(120, 240, 191, 41))
+        self.login_link.setText("Already registered - Login")
+        self.login_link.setObjectName("login_link")
         self.signup_button = QtWidgets.QPushButton(Login_Window)
-        self.signup_button.setGeometry(QtCore.QRect(60, 140, 251, 27))
+        self.signup_button.setGeometry(QtCore.QRect(90, 170, 251, 41))
         self.signup_button.setAutoFillBackground(False)
         self.signup_button.setStyleSheet("QPushButton{\n"
-                                         "    background-color: rgb(79, 159, 239);\n"
-                                         "    border-color: rgb(57, 115, 173);\n"
-                                         "    \n"
-                                         "    \n"
-                                         "    font: 11pt \"Roboto\";\n"
-                                         "}")
-        self.signup_button.setCheckable(False)
-        self.signup_button.setFlat(False)
+"    color: #fff;\n"
+"    font: 11pt \"Roboto\";\n"
+"background-color : rgb(79, 159, 238);\n"
+"    border-color: blue;\n"
+"border-radius: 4px;       \n"
+"  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);\n"
+"  transition: all 0.3s cubic-bezier(.25,.8,.25,1);\n"
+"\n"
+"\n"
+"\n"
+"       \n"
+"}")
         self.signup_button.setObjectName("signup_button")
-        self.back_link = QtWidgets.QCommandLinkButton(Login_Window)
-        self.back_link.setGeometry(QtCore.QRect(130, 200, 101, 31))
-        self.back_link.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
-        self.back_link.setAutoFillBackground(False)
-        self.back_link.setStyleSheet("QCommandLinkButton\n"
-                                     "{\n"
-                                     "    qproperty-icon : None;\n"
-                                     "    color: rgb(65, 130, 195);\n"
-                                     "    text-align : center;\n"
-                                     "    font: 11pt \"Roboto\";\n"
-                                     "}")
-        self.back_link.setObjectName("back_link")
-        self.login_label = QtWidgets.QLabel(Login_Window)
-        self.login_label.setGeometry(QtCore.QRect(120, 10, 131, 41))
-        self.login_label.setStyleSheet("QLabel\n"
-                                       "{\n"
-                                       "    color: rgb(67, 135, 202);\n"
-                                       "    \n"
-                                       "    \n"
-                                       "    font: 14pt \"Roboto\";\n"
-                                       "}")
-        self.login_label.setObjectName("login_label")
-        self.signup_label = QtWidgets.QLabel(Login_Window)
-        self.signup_label.setGeometry(QtCore.QRect(120, 10, 131, 41))
-        self.signup_label.setStyleSheet("QLabel\n"
-                                        "{\n"
-                                        "    color: rgb(69, 139, 208);\n"
-                                        "    \n"
-                                        "    font:  14pt \"Roboto\";\n"
-                                        "}")
-        self.signup_label.setObjectName("signup_label")
+        self.login_button.raise_()
+        self.password.raise_()
+        self.username.raise_()
+        self.login_label.raise_()
+        self.signup_link.raise_()
+        self.signup_label.raise_()
+        self.login_link.raise_()
+        self.signup_button.raise_()
 
         self.retranslateUi(Login_Window)
         QtCore.QMetaObject.connectSlotsByName(Login_Window)
@@ -258,21 +270,19 @@ class Ui_Login_Window(object):
         _translate = QtCore.QCoreApplication.translate
         Login_Window.setWindowTitle(_translate("Login_Window", "Login/SignUp"))
         self.login_button.setText(_translate("Login_Window", "Login"))
-        self.username.setPlaceholderText(_translate("Login_Window", "                       Username"))
-        self.password.setPlaceholderText(_translate("Login_Window", "                       Password"))
-        self.new_user_link.setText(_translate("Login_Window", "New User ?"))
-        self.signup_button.setText(_translate("Login_Window", "Sign Up"))
-        self.back_link.setText(_translate("Login_Window", "      Back"))
-        self.login_label.setText(_translate("Login_Window", "      LOGIN"))
-        self.signup_label.setText(_translate("Login_Window", "    SIGN UP"))
+        self.username.setPlaceholderText(_translate("Login_Window", "Username"))
+        self.password.setPlaceholderText(_translate("Login_Window", "Password"))
+        self.login_label.setText(_translate("Login_Window", "Login"))
+        self.signup_label.setText(_translate("Login_Window", "Signup"))
+        self.signup_button.setText(_translate("Login_Window", "Signup"))
 
 
 if __name__ == "__main__":
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
     Login_Window = QtWidgets.QDialog()
     ui = Ui_Login_Window()
     ui.setupUi(Login_Window)
     Login_Window.show()
     sys.exit(app.exec_())
+
