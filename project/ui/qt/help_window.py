0a18b953a6af61780e36ea49a357ea8ba205d1ea
nishakhiyani <nishakhiyani123@gmail.com>
2017-03-24 12:00:32 +0530

    Updating Help window Ui.

--- project/ui/qt/help_window.py
+++ project/ui/qt/help_window.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'help_window.ui'
+# Form implementation generated from reading ui file 'ui/qt/help_window.ui'
 #
 # Created by: PyQt5 UI code generator 5.7
 #
@@ -91,7 +91,7 @@ class Ui_Help_Window(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Roboto\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\';\"> Khud ke daalo tips</span></p></body></html>"))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">To be updated</p></body></html>"))
         self.shortcut_edit.setHtml(_translate("Help_Window", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
